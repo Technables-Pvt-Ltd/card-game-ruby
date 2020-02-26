9a03fb4be7fdb3c03e5fbd9366dad01bbdb507db
Chandra <chandra@braindigit.com>
2020-02-26 12:35:37 +0545

    api route change

--- card-react/src/data/constants/apiroutes.js
+++ card-react/src/data/constants/apiroutes.js
@@ -1,3 +1,4 @@
+//export const ROUTE_API_URL = "http://localhost:3001/";
 export const ROUTE_API_URL = "http://172.18.13.234:3001/";
 export const ROUTE_API_INITDECK = "init"
 export const ROUTE_API_INITGAME = "initgame"
