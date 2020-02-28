8a768880aa283e6c6a565572b036e8a00af8f31f
Chandra <chandra@braindigit.com>
2020-02-29 02:10:52 +0545

    design update for board

--- card-react/src/components/controls/gameplayer/index.js
+++ card-react/src/components/controls/gameplayer/index.js
@@ -20,27 +20,38 @@ export class GamePlayer extends Component {
 
             <div className={`player-container ${this.player.position}`}>
                 <div className="row">
-                    {
-                        handClass.length === 0 && (
+                    {/* {
+                        handClass.length > 0 && (
                             <div className={`hand-pile pile  ${this.player.deckclass} ${handClass}`}>
                                 <span className="card-count">{this.player.discard_pile.length}</span>
                                 <span className="title">hand </span>
                             </div>
                         )
+                    } */}
+                    {
+                        handClass.length > 0 && (
+                            <div className={`discard-pile pile  ${this.player.deckclass}`}>
+                                <span className={`card-count ${this.player.deckclass}`}>{this.player.discard_pile.length}</span>
+                                <span className="title">discard </span>
+                            </div>
+                        )
                     }
-                    <div className={`discard-pile pile  ${this.player.deckclass}`}>
-                        <span className="card-count">{this.player.discard_pile.length}</span>
-                        <span className="title">discard </span>
+
+                    <div className={`player-data ${this.player.deckclass}`}>
+                    <span className={`card-count ${this.player.deckclass}`}>{this.player.health}</span>
+
+                        <span className="name-title">{this.player.name}</span>
                     </div>
 
-                    <div className={`player-data `}>
-                        <span className="title">{this.player.name}</span>
-                    </div>
+                    {
+                        handClass.length > 0 && (
 
-                    <div className={`deck-pile pile ${this.player.deckclass}`}>
-                        <span className="card-count">{this.player.deck_pile.length}</span>
-                        <span className="title">Deck </span>
-                    </div>
+                            <div className={`deck-pile pile ${this.player.deckclass}`}>
+                                <span className={`card-count ${this.player.deckclass}`}>{this.player.deck_pile.length}</span>
+                                <span className="title">Deck </span>
+                            </div>
+                        )
+                    }
 
 
                 </div>
@@ -48,7 +59,7 @@ export class GamePlayer extends Component {
                 {handClass.length > 0 && (
                     <div className="row">
                         <div className={`hand-pile hand  ${this.player.deckclass} ${handClass}`}>
-                             <span className="title">hand </span>
+                            <span className="title">hand </span>
                         </div>
                     </div>
                 )}
